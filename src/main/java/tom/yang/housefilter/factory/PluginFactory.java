8fae9246e5d38adec7b4065f8a06cf2e292c01db
tom.yang <tom.yang@netis.com.cn>
2016-05-04 17:30:48 +0800

    Plugin support an string parameter.

--- src/main/java/tom/yang/housefilter/factory/PluginFactory.java
+++ src/main/java/tom/yang/housefilter/factory/PluginFactory.java
@@ -1,5 +1,7 @@
 package tom.yang.housefilter.factory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
 
 import org.reflections.Reflections;
@@ -10,37 +12,41 @@ import tom.yang.housefilter.weight.ICellWeight;
 
 public class PluginFactory<T> {
 
-	public static PluginFactory<WeightCondition> CONDITION_FACTORY = new PluginFactory<WeightCondition>(
-			WeightCondition.class);
+    public static PluginFactory<WeightCondition> CONDITION_FACTORY = new PluginFactory<WeightCondition>(
+            WeightCondition.class);
 
-	public static PluginFactory<IHouseRowFilter> FILTER_FACTORY = new PluginFactory<IHouseRowFilter>(
-			IHouseRowFilter.class);
+    public static PluginFactory<IHouseRowFilter> FILTER_FACTORY = new PluginFactory<IHouseRowFilter>(
+            IHouseRowFilter.class);
 
-	public static PluginFactory<ICellWeight> WEIGHT_FACTORY = new PluginFactory<ICellWeight>(ICellWeight.class);
+    public static PluginFactory<ICellWeight> WEIGHT_FACTORY = new PluginFactory<ICellWeight>(ICellWeight.class);
 
-	private final Reflections reflections;
+    private final Reflections reflections;
 
-	final Class<T> typeParameterClass;
+    final Class<T> typeParameterClass;
 
-	{
-		reflections = new Reflections("tom.yang.housefilter");
-	}
+    {
+        reflections = new Reflections("tom.yang.housefilter");
+    }
 
-	public PluginFactory(Class<T> typeParameterClass) {
-		this.typeParameterClass = typeParameterClass;
-	}
+    public PluginFactory(Class<T> typeParameterClass) {
+        this.typeParameterClass = typeParameterClass;
+    }
 
-	public T create(String name, String... args) throws InstantiationException, IllegalAccessException {
-		Set<Class<? extends T>> types = reflections.getSubTypesOf(typeParameterClass);
-		for (Class<? extends T> type : types) {
-			if (type.getName().endsWith(name)) {
-				if (args.length > 0) {
+    public T create(String name, String arg)
+            throws InstantiationException, IllegalAccessException, NoSuchMethodException,
+            SecurityException, IllegalArgumentException, InvocationTargetException {
+        Set<Class<? extends T>> types = reflections.getSubTypesOf(typeParameterClass);
+        for (Class<? extends T> type : types) {
+            if (type.getName().endsWith(name)) {
+                if (arg != null) {
+                    Constructor<? extends T> constructor = type.getConstructor(String.class);
+                    return constructor.newInstance(arg);
+                } else {
+                    return type.newInstance();
+                }
+            }
+        }
+        return null;
+    }
 
-				} else {
-					return type.newInstance();
-				}
-			}
-		}
-		return null;
-	}
 }
--- src/test/java/tom/yang/housefilter/PluginFactoryTest.java
+++ src/test/java/tom/yang/housefilter/PluginFactoryTest.java
@@ -1,25 +1,50 @@
 package tom.yang.housefilter;
 
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import tom.yang.housefilter.condition.HasCellValueCondition;
+import tom.yang.housefilter.condition.WeightCondition;
+import tom.yang.housefilter.core.ConditionContext;
+import tom.yang.housefilter.core.HouseCell;
+import tom.yang.housefilter.core.HouseRow;
 import tom.yang.housefilter.factory.PluginFactory;
 import tom.yang.housefilter.rowfilter.FirstRowIndexFilter;
 import tom.yang.housefilter.rowfilter.IHouseRowFilter;
 
 public class PluginFactoryTest {
 
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
-	@Test
-	public void testCreateFirstRowFilter() throws InstantiationException, IllegalAccessException {
-		IHouseRowFilter filter = PluginFactory.FILTER_FACTORY.create("FirstRowIndexFilter");
-		Assert.assertTrue(filter instanceof FirstRowIndexFilter);
-	}
+    @Test
+    public void testCreateFirstRowFilter()
+            throws InstantiationException, IllegalAccessException, NoSuchMethodException,
+            SecurityException, IllegalArgumentException, InvocationTargetException {
+        IHouseRowFilter filter = PluginFactory.FILTER_FACTORY.create("FirstRowIndexFilter", null);
+        Assert.assertTrue(filter instanceof FirstRowIndexFilter);
+    }
+
+    @Test
+    public void testCreateHasCellValueCondition()
+            throws InstantiationException, IllegalAccessException, NoSuchMethodException,
+            SecurityException, IllegalArgumentException, InvocationTargetException {
+        WeightCondition condition = PluginFactory.CONDITION_FACTORY.create("HasCellValueCondition",
+                "condition");
+        Assert.assertTrue(condition instanceof HasCellValueCondition);
+        ConditionContext ctx = new ConditionContext();
+        HouseRow row = new HouseRow();
+        HouseCell cell = new HouseCell("condition", 1);
+        row.getCells().add(cell);
+        ctx.setRow(row);
+
+        Assert.assertTrue(condition.match(ctx));
+    }
 
 
 }
